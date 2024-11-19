321172d151929c1ca1251122a81effde2757f9a8
Dafne <dafne.godoy.h@gmail.com>
2024-11-19 01:36:29 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // Definir las listas
+  console.log("prueba de nombres aleatorios");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for (let i of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        let domain = i + a + n + '.com'; //todo valor + .com
+       console.log(domain); 
+      }
+    }
+  }
+
 };
